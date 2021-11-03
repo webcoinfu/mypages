3b32761131591407b6fe0373c127445433c00291
Passawat Noraman <hi@notrealpaz.me>
2021-11-04 04:52:54 +0700

    Add contact info (ref. xpaw)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,164 +1,195 @@
 <script context="module">
-	export const prerender = true;
+  export const prerender = true;
+</script>
+
+<script>
+  import { onMount } from "svelte";
+  onMount(async () => {
+    if (navigator.webdriver || !location.hostname) return;
+		const text = '\x68\x69\x40' + location.hostname;
+		const element = document.querySelector( 'a[href="/email/hi/at/this/domain"]' );
+		element.textContent = text;
+		element.href = `\x6d\x61\x69\x6C\x74\x6F\x3A${text}`;
+  });
 </script>
 
 <svelte:head>
-	<link rel="preload" as="image" href="/bg1.png">
-	<link rel="preload" as="image" href="/bg2.png">
-	<link rel="preload" as="image" href="/bg3.png">
+  <link rel="preload" as="image" href="/bg1.png" />
+  <link rel="preload" as="image" href="/bg2.png" />
+  <link rel="preload" as="image" href="/bg3.png" />
 </svelte:head>
 
 <div class="shutter1" />
 <main>
-	<div class="flex full videowarp">
-		<div class="bg" />
-		<div class="blurbar" />
-		<h1>NotRealPaz</h1>
-	</div>
-	<div class="flex full" />
-	<p>
-		© {new Date().getFullYear()} by NotRealPaz
-	</p>
+  <div class="flex full videowarp">
+    <div class="bg" />
+    <div class="blurbar" />
+    <h1 class="title">NotRealPaz</h1>
+  </div>
+  <div class="flex full">
+    <div>
+      <h1>Passawat Noraman</h1>
+      <p>Full-stack developer</p>
+      <p class="link">
+        <img src="/twitter.svg" alt="twitter icon">
+        <a href="https://twitter.com/NotRealPaz" rel="me">NotRealPaz</a>
+      </p>
+      <p class="link">
+        <img src="/github.svg" alt="github icon">
+        <a href="https://github.com/NotRealPaz" rel="me">NotRealPaz</a>
+      </p>
+      <p class="link">
+        <img src="/email.svg" alt="github icon">
+        <a href="/email/hi/at/this/domain">hi at this domain</a>
+      </p>
+    </div>
+  </div>
+  <p class="text-center">
+    © {new Date().getFullYear()} by NotRealPaz
+  </p>
 </main>
 
 <style>
-	@keyframes bgchange {
-		from {
-			content: url("/bg1.png");
-		}
+  @keyframes bgchange {
+    from {
+      content: url("/bg1.png");
+    }
 
-		96% {
-			content: url("/bg1.png");
-		}
+    96% {
+      content: url("/bg1.png");
+    }
 
-		98% {
-			content: url("/bg2.png");
-		}
+    98% {
+      content: url("/bg2.png");
+    }
 
-		to {
-			content: url("/bg3.png");
-		}
-	}
+    to {
+      content: url("/bg3.png");
+    }
+  }
 
-	.videowarp {
-		position: relative;
-		overflow: hidden;
-		width: 100%;
-		height: 100vh;
-	}
+  .videowarp {
+    position: relative;
+    overflow: hidden;
+    width: 100%;
+    height: 100vh;
+  }
 
-	div.bg {
-		position: absolute;
-		width: 150%;
-		/* min-height: 100vh; */
-		z-index: 1;
-		filter: blur(4px) brightness(0.25);
-		content: url("/bg1.png");
-		animation: bgchange 2s forwards infinite alternate;
-	}
+  div.bg {
+    position: absolute;
+    width: 150%;
+    /* min-height: 100vh; */
+    z-index: 1;
+    filter: blur(4px) brightness(0.25);
+    content: url("/bg1.png");
+    animation: bgchange 2s forwards infinite alternate;
+  }
 
-	.blurbar {
-		position: absolute;
-		width: 100%;
-		height: 100vh;
-		background-image: linear-gradient(
-			rgba(0, 0, 0, 0) 70%,
-			var(--bg-color) 100%
-		);
-		z-index: 2;
-	}
+  .blurbar {
+    position: absolute;
+    width: 100%;
+    height: 100vh;
+    background-image: linear-gradient(
+      rgba(0, 0, 0, 0) 70%,
+      var(--bg-color) 100%
+    );
+    z-index: 2;
+  }
 
-	main {
-		text-align: center;
-		margin: 0 auto;
-		max-width: 1024px;
-	}
+  main {
+    margin: 0 auto;
+    max-width: 1024px;
+  }
 
-	.flex {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
+  .flex {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
-	.full {
-		width: 100%;
-		height: 100vh;
-	}
+  .full {
+    width: 100%;
+    height: 100vh;
+  }
 
-	h1 {
-		position: absolute;
-		color: #0084ff;
-		font-size: 10vw;
-		font-weight: 200;
-		z-index: 3;
-	}
+  h1.title {
+    position: absolute;
+    font-size: 10vw;
+    font-weight: 200;
+    z-index: 3;
+  }
 
-	p {
-		font-size: 1em;
-	}
+	.link img {
+    vertical-align: bottom;
+    margin-right: 5px;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 
-	.shutter1 {
-		position: fixed;
-		top: 0;
-		left: 0;
-		right: 0;
-		bottom: 0;
-		background-color: #000;
-		z-index: 10000;
-		animation: byeShutter 2s forwards;
-	}
+  .shutter1 {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #000;
+    z-index: 10000;
+    animation: byeShutter 2s forwards;
+  }
 
-	.shutter1:before {
-		content: "";
-		position: absolute;
-		top: 0;
-		left: 0;
-		bottom: 0;
-		margin: auto;
-		width: 0;
-		height: 1px;
-		animation: shutterOpen 2s forwards;
-	}
+  .shutter1:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    margin: auto;
+    width: 0;
+    height: 1px;
+    animation: shutterOpen 2s forwards;
+  }
 
-	@keyframes byeShutter {
-		80% {
-			opacity: 1;
-		}
+  @keyframes byeShutter {
+    80% {
+      opacity: 1;
+    }
 
-		to {
-			opacity: 0;
-			display: none;
-			z-index: -1;
-		}
-	}
+    to {
+      opacity: 0;
+      display: none;
+      z-index: -1;
+    }
+  }
 
-	@keyframes shutterOpen {
-		0% {
-			width: 0;
-			height: 2px;
-			background-color: rgb(0, 132, 255);
-		}
+  @keyframes shutterOpen {
+    0% {
+      width: 0;
+      height: 2px;
+      background-color: rgb(0, 132, 255);
+    }
 
-		50% {
-			width: 100%;
-			height: 5%;
-		}
+    50% {
+      width: 100%;
+      height: 5%;
+    }
 
-		90% {
-			width: 100%;
-			height: 100%;
-		}
+    90% {
+      width: 100%;
+      height: 100%;
+    }
 
-		to {
-			width: 100%;
-			height: 100%;
-			background-color: var(--bg-color);
-		}
-	}
+    to {
+      width: 100%;
+      height: 100%;
+      background-color: var(--bg-color);
+    }
+  }
+
+  .text-center {
+    text-align: center;
+  }
 </style>
